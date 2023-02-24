27ca0b970a7e3647b4a0dc38a0709bee6d2d2d8c
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-02-24 15:02:29 -0300

    created request insert date

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,20 @@
 import fastify from 'fastify'
 import { knex } from './database'
 
+import crypto from 'node:crypto'
+
 const app = fastify()
 
 app.get('/hello', async (request, response) => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transactions = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*')
 
-  return tables
+  return transactions
 })
 
 app
