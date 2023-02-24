7a346dada9fad29eccd9354e6b48c43788f8bb0f
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-02-24 09:56:02 -0300

    add knex and sqlite data base

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
-app.get('/hello', (request, response) => {
-  return 'Hello word'
+app.get('/hello', async (request, response) => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
 })
 
 app
