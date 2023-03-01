428bae6742ee102ebd7fd664f6a441f922ef28ea
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-03-01 11:27:03 -0300

    added validation for data body with zod

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,10 @@ import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.register(transactionsRoutes)
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
+})
+
 app
   .listen({
     port: env.PORT,
