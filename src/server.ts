f681ef32108311b4d0162b6dd4f5dc497cc1c6cd
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-03-01 12:58:03 -0300

    added comment code error

--- src/server.ts
+++ src/server.ts
@@ -19,6 +19,18 @@ app.register(transactionsRoutes, {
   process.exit(1)
 }) */
 
+/* fastify.setErrorHandler(function (error, request, reply) {
+  if (error instanceof Fastify.errorCodes.FST_ERR_BAD_STATUS_CODE) {
+    // Log error
+    this.log.error(error)
+    // Send error response
+    reply.status(500).send({ ok: false })
+  } else {
+    // fastify will use parent error handler to handle this
+    reply.send(error)
+  }
+}) */
+
 app.listen(
   {
     port: env.PORT,
