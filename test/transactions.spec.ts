7f964e4ca0903b170aa3e370a68f235b80d1372b
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-03-08 16:39:14 -0300

    create database test

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,5 +1,6 @@
-import { expect, it, beforeAll, afterAll, describe } from 'vitest'
+import { expect, it, beforeAll, afterAll, describe, beforeEach } from 'vitest'
 import request from 'supertest'
+import { execSync } from 'node:child_process'
 import { app } from '../src/app'
 
 describe('Transactions routes', () => {
@@ -9,6 +10,11 @@ describe('Transactions routes', () => {
 
   afterAll(async () => {
     await app.close() // fechar aplicação após terminar todos os testes
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
   })
 
   it('should be able to create a new transaction', async () => {
