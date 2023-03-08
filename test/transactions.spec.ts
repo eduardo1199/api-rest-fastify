5387a05645e92b44b4f4949153e6eab738e50b26
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-03-08 16:18:46 -0300

    test list transactions

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test, beforeAll, afterAll, describe } from 'vitest'
+import { expect, it, beforeAll, afterAll, describe } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -11,7 +11,7 @@ describe('Transactions routes', () => {
     await app.close() // fechar aplicação após terminar todos os testes
   })
 
-  test('should be able to create a new transaction', async () => {
+  it('should be able to create a new transaction', async () => {
     const response = await request(app.server).post('/transactions').send({
       title: 'create new transaction',
       amount: 5000,
@@ -19,5 +19,28 @@ describe('Transactions routes', () => {
     })
 
     expect(response.statusCode).toEqual(201)
+  })
+
+  it('should be able to list all transaction', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'create new transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactions = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+
+    expect(listTransactions.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'create new transaction',
+        amount: 5000,
+      }),
+    ])
   })
 })
