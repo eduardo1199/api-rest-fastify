b98e74a3361b36e361d137c1e80b083454732d62
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-03-08 16:06:54 -0300

    describe test

---
+++ test/transactions.spec.ts
@@ -0,0 +1,23 @@
+import { expect, test, beforeAll, afterAll, describe } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready() // espere que os plugins carregem na aplicação
+  })
+
+  afterAll(async () => {
+    await app.close() // fechar aplicação após terminar todos os testes
+  })
+
+  test('should be able to create a new transaction', async () => {
+    const response = await request(app.server).post('/transactions').send({
+      title: 'create new transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
